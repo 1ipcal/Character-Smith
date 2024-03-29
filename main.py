3f4beb8992a5395dbeb8de238804f4fcbecf1adc
ryan-bachir <ryan.s.bachir@gmail.com>
2024-03-29 14:15:40 -0400

    additional testing for dropdown menus

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from tkinter import *
 class Character:
     def __init__(self, root) -> None:
         self.root = root
+        option = StringVar(root)
+        class_file = open('ASSETS\CLASSES.txt', 'r')
+        class_options = [line for line in class_file.readlines()]
+        race_file = open('ASSETS\RACES.txt', 'r')
+        race_options = [line for line in race_file.readlines()]
+
         character_name_label = Label(self.root, text='Name')
         character_name_entry = Entry(self.root)
 
         character_class_label = Label(self.root, text='Class')
-        character_class_entry = Entry(self.root)
+        character_class_menu = OptionMenu(self.root, option, *class_options)
 
         character_level_label = Label(self.root, text='Level')
         character_level_entry = Entry(self.root)
@@ -23,7 +29,7 @@ class Character:
         character_name_button.pack()
         
         character_class_label.pack()
-        character_class_entry.pack()
+        character_class_menu.pack()
 
     def lock_button(self, entry, button):
         # if button is enabled, disabled it and recess it. otherwise do the opposite
